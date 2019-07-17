9c8b4261a47d0f300a49a4b6b1ad75bdfb77f528
Vivek srivastava <vivek.srivastava@reddoorz.com>
2019-07-17 20:36:59 +0530

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190619111410) do
+ActiveRecord::Schema.define(version: 20190717150431) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "age",        limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "test_demo", id: false, force: :cascade do |t|
+    t.string "name", limit: 255
+  end
+
+  create_table "test_demo2", id: false, force: :cascade do |t|
+    t.integer "id",   limit: 4,   default: 0, null: false
+    t.string  "name", limit: 255
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "image",      limit: 255
   end
 
 end
